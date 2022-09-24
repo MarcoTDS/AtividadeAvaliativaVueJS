1525be69bac9f1e4d325999d0e5d811e706c3ca3
MARCO AURÉLIO DIAS SOUZA <75531639+MarcoTDS@users.noreply.github.com>
2022-09-24 12:11:39 -0300

    Add files via upload

---
+++ lojaTintaEntrega/src/App.vue
@@ -0,0 +1,110 @@
+<template>
+
+  <header>
+    <hr>
+    <p>Color Tintas</p>
+    <hr>
+  </header>
+  <section>
+    <div>
+      <p>Qual a área que deseja pintar?(m2)</p>
+      <input type="number" v-model="area">
+      <button v-on:click='calcular()'>Calcular</button>
+    </div>
+  </section>
+  <section class='resultados'>
+    <div class='total'>
+      <p>Quantidade de latas de 18 litros: {{latas}}</p>
+      <p>Quantidade de galões de 3,6 litros: {{galoes}}</p>
+      <p>Valor total: {{total}}</p>
+    </div>
+  </section>
+ 
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      area:0,
+      latas:0,
+      galoes:0,
+      total:0
+    }
+  },
+  methods:{
+    calcular(){
+     let litros = this.area/6;
+     if(litros%18==0){
+        this.latas = litros/18;
+        this.galoes = 0;
+        this.total = this.latas*80;
+      }
+      else if(litros%18!=0){
+        this.latas = Math.floor(litros/18);
+        this.galoes = Math.ceil((litros%18)/3.6);
+        this.total = (this.latas*80)+(this.galoes*25);
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+  font-size: 30px;
+}
+header{
+  background-image: linear-gradient(to right, #5b28de 0%, #a6c1ee 51%, #9a78f2 100%);
+  position: static;
+}
+input{
+  width: 40vw;
+  height: 25px;
+  border-radius: 5px;
+  border: 1px solid rgb(17, 93, 164);
+}
+button:hover{
+  filter: drop-shadow(0 0 0.75rem #5b28de);
+  transition: 0.2s ease-in-out;
+}
+button{
+  width: 60px;
+  height: 25px;
+  margin-left: 5px;
+  border-radius:5px;
+  border: 1px solid rgb(17, 93, 164);
+  background-color: #a6c1ee;
+  color: #2c3e50;
+  cursor: pointer;
+  text-align: center;
+}
+.resultados{
+  margin:10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.total{
+  display: flex;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+}
+.total p{
+  font-size: 15px;
+   color: #2c3e50;
+  border-radius:7px;
+  border: 1px solid rgb(17, 93, 164);
+  width: 50vw;
+}
+</style>
---
+++ pizzometroEntrega/src/App.vue
@@ -0,0 +1,112 @@
+<template>
+  <h2> Pizzaria do seu tadeu </h2>
+  <div class='pessoas'>
+        <h1>Quantidade de adultos</h1>
+        <input type="number" placeholder="Digite a quantidade de adultos" v-model='adultos'>
+        <h1>Quantidade de crianças</h1>
+        <input type="number" placeholder="Digite a quantidade de crianças" v-model="criancas">
+   </div>
+  <button v-on:click="calcular()">Calcular</button>
+  <p>
+    Quantidade de pizzas gigantes(12):{{quantidadeDePizza12}} 
+  </p>
+  <p>
+    Quantidade de pizzas média(10):{{quantidadeDePizza10}} 
+  </p>
+  <p>
+    Quantidade de pizzas pequena(8):{{quantidadeDePizza8}} 
+  </p>
+  
+</template>
+<script>
+  export default {
+    name: 'App',
+    data(){
+        return{
+            adultos: 0,
+            criancas: 0,
+            quantidadeDePedacosAdulto:0,
+            quantidadeDePedacosCrianca:0,
+            quantidadeDePizza12 : 0,
+            quantidadeDePizza10 : 0,
+            quantidadeDePizza8 : 0,
+        }
+    },
+    methods : {
+        calcular(){
+            let resto;
+            
+            this.quantidadeDePedacosAdulto = this.adultos * 3;
+            this.quantidadeDePedacosCrianca = this.criancas * 2;
+            let totalDePedacos = (this.quantidadeDePedacosAdulto) + (this.quantidadeDePedacosCrianca);
+
+
+            this.quantidadeDePizza12 = parseInt(totalDePedacos / 12);
+            resto = totalDePedacos % 12;
+            this.quantidadeDePizza10 = parseInt(resto / 10);
+            resto = resto % 10;
+             this.quantidadeDePizza8 = parseInt(resto / 8);
+            resto = resto % 8;
+
+            if(resto > 0){
+                this.quantidadeDePizza8++;
+            }
+        }
+    }
+
+  }
+ 
+
+</script>
+<style scoped>
+
+  h2{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    background-image: linear-gradient(to right, #08fa91, #0b51f5);
+    text-align: center;
+    color: #ffffff;
+  }
+  button{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    margin-left: 20px;
+    margin-right: 20px;
+    padding: 20px;
+    width: 100px;
+    border: 1px solid #08fa91;
+    border-radius: 5px;
+   background-color: aqua;
+    color: #ffffff;
+    font-size: 15px;
+    font-weight: bold;
+  }
+   .pessoas{
+        display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+        margin-top: 20px;
+        margin-left: 20px;
+        margin-right: 20px;
+        padding: 20px;
+        width: 50vw;
+        border: 1px solid #08fa91;
+        border-radius: 10px;
+    }
+    .pessoas h1{
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        text-align: center;
+        color: #0a1ed4;
+    }
+    .pessoas input{
+        width: 100%;
+        height: 40px;
+        border: 1px solid #08fa91;
+        border-radius: 10px;
+        padding: 10px;
+        font-size: 20px;
+    }
+
+</style>
